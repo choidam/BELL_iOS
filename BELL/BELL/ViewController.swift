84ff8ee603a6bec4cad8266a52ad2e1e5f9bb147
Choieunji0114 <cheunji8209@naver.com>
2020-05-03 14:01:56 +0900

    Upload BELL project

---
+++ BELL/BELL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BELL
+//
+//  Created by 최은지 on 03/05/2020.
+//  Copyright © 2020 최은지. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
