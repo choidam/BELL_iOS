ef11532f0967843b53e6a84355e7c7ce971340ea
Choieunji0114 <cheunji8209@naver.com>
2020-06-05 03:13:04 +0900

    connect API in mapView

--- BELL/BELL/Sources/ViewControllers/MapVC/MapViewController.swift
+++ BELL/BELL/Sources/ViewControllers/MapVC/MapViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
-import FloatingPanel
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController, FloatingPanelControllerDelegate {
-
+class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager = CLLocationManager()
+    var currentLocation: CLLocation!
+    
+    @IBOutlet weak var InfoView: UIView! // 미세먼지 정보 view
+    
+    var aqiDataSet = [AqiResponseString]() // aqi dataset
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager = manager
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            currentLocation = locationManager.location
+        }
+    }
+    
+    // MARK : 위치 허용 선택했을 때 처리
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined :
+            manager.requestWhenInUseAuthorization()
+            break
+        case .authorizedWhenInUse:
+            self.firstSetting()
+            break
+        case .authorizedAlways:
+            self.firstSetting()
+            break
+        case .restricted :
+            break
+        case .denied :
+            break
+        default:
+            break
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,14 +55,89 @@ class MapViewController: UIViewController, FloatingPanelControllerDelegate {
         self.mapView.showsUserLocation = true
         self.mapView.setUserTrackingMode(.follow, animated: true)
         
-        let fpc = FloatingPanelController()
-        fpc.delegate = self
-        
-        guard let contentVC = storyboard?.instantiateViewController(identifier: "fpc_content") as? MapContentViewController else { return }
-        
-        fpc.set(contentViewController: contentVC)
-        fpc.addPanel(toParent: self)
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        locationManager.startMonitoringSignificantLocationChanges()
         
     }
-
+    
+    // MARK : 위치 받아오기 에러 처리
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if CLLocationManager.locationServicesEnabled() {
+            if CLLocationManager.authorizationStatus() == .denied || CLLocationManager.authorizationStatus() == .restricted {
+                let alert = UIAlertController(title: "오류 발생", message: "위치 서비스 기능이 꺼져있음", preferredStyle: .alert)
+                let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                alert.addAction(okAction)
+                self.present(alert, animated: true, completion: nil)
+            } else {
+                locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                locationManager.delegate = self
+                locationManager.requestWhenInUseAuthorization()
+            }
+        } else {
+            let alert = UIAlertController(title: "오류 발생", message: "위치 서비스 제공 불가", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        // 이미 허용인 경우 처리
+        if CLLocationManager.authorizationStatus() == .authorizedAlways || CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            self.firstSetting()
+        }
+    }
+    
+    // MARK : find location and address
+    func firstSetting(){
+        self.currentLocation = locationManager.location
+        print(self.currentLocation.coordinate.latitude)
+        print(self.currentLocation.coordinate.longitude)
+        self.findAddr(lat: self.currentLocation.coordinate.latitude, long: self.currentLocation.coordinate.longitude)
+    }
+    
+    // MARK : 위도, 경도에 따른 주소 찾기
+    func findAddr(lat: CLLocationDegrees, long: CLLocationDegrees){
+        let findLocation = CLLocation(latitude: lat, longitude: long)
+        let geocoder = CLGeocoder()
+        let locale = Locale(identifier: "Ko-kr")
+        
+        geocoder.reverseGeocodeLocation(findLocation, preferredLocale: locale, completionHandler: {(placemarks, error) in
+            if let address: [CLPlacemark] = placemarks {
+                if let name: String = address.last?.name {
+                    //                    self.addressLabel.text = name // 남가좌동 50-3
+                }
+                if let area: String = address.last?.locality{
+                    print("findaddress in map: ", area) // 서대문구
+                    self.connectAqiAPI(region: area) // 미세먼지 API 연결
+                }
+            }
+        })
+    }
+    
+    // MARK : 미세먼지 API 연결
+    func connectAqiAPI(region: String){
+        let aqiURLString = AqiService.shared.makeAqiAddress(region: region)
+        let aqiURL = URL(string: aqiURLString)!
+        
+        do {
+            let responseString = try String(contentsOf: aqiURL)
+            guard let data = responseString.data(using: .utf8) else { return }
+            let decoder = JSONDecoder()
+            if let object = try? decoder.decode(AqiResponseString.self, from: data) {
+                self.aqiDataSet = [object] as! [AqiResponseString]
+                print("@@@@")
+                print(self.aqiDataSet)
+            }
+        } catch let e as NSError {
+            print(e.localizedDescription)
+        }
+    }
+    
+    
+    
+    
 }
